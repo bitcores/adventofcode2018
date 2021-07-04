47790dc7f143dfb4a6630dd3fd6148e99929a6dd
bitcores <bitcores@gmail.com>
2021-07-04 10:22:15 +0900

    Add files via upload

--- d6.py
+++ d6.py
@@ -1,6 +1,7 @@
 import time
-from copy import deepcopy
 import operator
+from PIL import Image, ImageDraw
+from math import floor
 st = time.time()
 
 inputfile = "input6.txt"
@@ -25,6 +26,7 @@ inp = readinput()
 #inp = splitinput()
 
 ## Solve problem
+# all coords will be in (y, x) format
 scoords = dict()
 gr = {}
 gs = {}
@@ -33,11 +35,8 @@ cnrs = [[9999,9999] , [0,0]]
 n = 1
 tg = {}
 target = 10000
-
-class coord:
-    def __init__(self, n=None, cs=0):
-        self.nearest = n
-        self.csum = cs
+targreg = []
+colors = {".": (0,0,0)}
 
 def calcsum(y,x):
     cs = 0
@@ -47,62 +46,92 @@ def calcsum(y,x):
 
 for p in inp:
     c = p.split(", ")
-    c[0], c[1] = int(c[0]), int(c[1])
+    x, y = int(c[0]), int(c[1])
 
-    if c[0] < cnrs[0][0]:
-        cnrs[0][0] = c[0]-1
-    if c[1] < cnrs[0][1]:
-        cnrs[0][1] = c[1]-1
-    if c[0] > cnrs[1][0]:
-        cnrs[1][0] = c[0]+1
-    if c[1] > cnrs[1][1]:
-        cnrs[1][1] = c[1]+1
+    if y < cnrs[0][0]:
+        cnrs[0][0] = y-1
+    if x < cnrs[0][1]:
+        cnrs[0][1] = x-1
+    if y > cnrs[1][0]:
+        cnrs[1][0] = y+1
+    if x > cnrs[1][1]:
+        cnrs[1][1] = x+1
     
-    scoords[n] = ((c[1], c[0]))
+    scoords[n] = (y, x)
     gs[n] = 1
     n += 1
 
 for sc in scoords:
-    if not scoords[sc][1] in gr:
-        gr[scoords[sc][1]] = {}
-    if not scoords[sc][0] in gr[scoords[sc][1]]:
-        gr[scoords[sc][1]][scoords[sc][0]] = coord(sc, calcsum(scoords[sc][1], scoords[sc][0]))
+    if not scoords[sc][0] in gr:
+        gr[scoords[sc][0]] = {}
+    if not scoords[sc][1] in gr[scoords[sc][0]]:
+        gr[scoords[sc][0]][scoords[sc][1]] = sc
+        if calcsum(scoords[sc][0], scoords[sc][1]) < target:
+            targreg.append((scoords[sc][0], scoords[sc][1]))
+
+colordiv = floor((255 / (len(gs) / 3)) / 1.5)
+r, g, b = 255, 0, 0
+for c in gs.keys(): 
+    colors[c] = (r, g, b)
+    if r > 0:
+        r -= colordiv
+        if r < 0:
+            r = 0
+        g += colordiv
+        if g > 255:
+            g = 255
+    else:
+        g -= colordiv
+        if g < 0:
+            g = 0
+        b += colordiv
+        if b > 255:
+            b = 255
+
+#print(colors)
+
 
 #print(gr)
 #print(cnrs)
 
 def printgr():
-    for y in range(cnrs[0][0], cnrs[1][1]+1):
+    for x in range(cnrs[0][0], cnrs[1][0]+1):
         line = ""
-        for x in range(cnrs[0][1], cnrs[1][0]+1):
+        for y in range(cnrs[0][1], cnrs[1][1]+1):
             if not y in gr or not x in gr[y]:
                 line += " "
             else:
-                line += str(gr[y][x].nearest)
+                line += str(gr[y][x])
         print(line)
     
 def writegr():
-    f = open("d6out.txt", "w")
-    for y in range(cnrs[0][0], cnrs[1][1]+1):
-        line = ""
-        for x in range(cnrs[0][1], cnrs[1][0]+1):
+    yo = cnrs[0][0]
+    xo = cnrs[0][1]
+    img = Image.new("RGB", (cnrs[1][0]-yo+1, cnrs[1][1]-xo+1), (255, 255, 255))
+    for y in range(cnrs[0][0], cnrs[1][0]+1):
+        for x in range(cnrs[0][1], cnrs[1][1]+1):
             if not y in gr or not x in gr[y]:
-                line += " "
+                img.putpixel((y-yo, x-xo), (255, 255, 255))
             else:
-                line += str(gr[y][x].nearest)
-        f.write(line + "\n")
-    f.close()
+                img.putpixel((y-yo, x-xo), colors[gr[y][x]])
+
+    for ta in targreg:
+        inv = colors[gr[ta[0]][ta[1]]]
+        inv = (255-inv[0],255-inv[1],255-inv[2])
+        img.putpixel((ta[0]-yo, ta[1]-xo), inv)
+
+    img.save("d6out.png")
 
 def fillround(poslist, mag):
     es = [ [0,1], [0,-1], [1,0], [-1,0] ]
     ngr = {}
 
     for pos in poslist:
-        ip = gr[pos[0]][pos[1]].nearest
+        ip = gr[pos[0]][pos[1]]
         for e in es:
             y, x = pos[0]+e[0], pos[1]+e[1]
 
-            if x < cnrs[0][0] or x > cnrs[1][0] or y < cnrs[0][1] or y > cnrs[1][1]:
+            if y < cnrs[0][0] or y > cnrs[1][0] or x < cnrs[0][1] or x > cnrs[1][1]:
                 # its gone to infinity
                 #print("infinite")
                 if not ip == ".":
@@ -118,12 +147,14 @@ def fillround(poslist, mag):
             if not y in ngr:
                 ngr[y] = {}
             if not x in ngr[y]:
-                ngr[y][x] = coord(ip, calcsum(y,x))
+                ngr[y][x] = ip
+                if calcsum(y,x) < target:
+                    targreg.append((y, x))
             else:
-                if ngr[y][x].nearest == ip:
+                if ngr[y][x] == ip:
                     continue
                 else:
-                    ngr[y][x].nearest = (".")
+                    ngr[y][x] = (".")
                     #print("contested square")
     
     return ngr
@@ -146,13 +177,12 @@ while not end:
     fillfrom.clear()
     for ys in ugr:
         for xs in ugr[ys]:
-            ip = ugr[ys][xs].nearest
-            cs = ugr[ys][xs].csum
+            ip = ugr[ys][xs]
             fillfrom.append((ys, xs))
             if not ys in gr:
                 gr[ys] = {}
             if not xs in gr[ys]:
-                gr[ys][xs] = coord(ip, cs)
+                gr[ys][xs] = ip
                 if ip in gs:
                     gs[ip] += 1
 
@@ -160,22 +190,14 @@ while not end:
     #printgr()
     #input()
 #printgr()
-#writegr()
+writegr()
 
 for x in infa:
     gs.pop(x)
 #print(gs)
 
 print("Largest area:> ", max(gs.items(), key=operator.itemgetter(1))[1])
-
-tsum = 0
-for ys in gr:
-    for xs in gr[ys]:
-        if gr[ys][xs].csum < target:
-            
-            tsum += 1
-
-print("Target region size:> ", tsum)
+print("Target region size:> ", len(targreg))
 
 ## Print runtime
 et = time.time()
